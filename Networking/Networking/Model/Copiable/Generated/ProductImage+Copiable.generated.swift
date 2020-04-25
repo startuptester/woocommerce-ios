ab55fd9880b26a99cbbc622002c7bf23b7da7ea7
Shiki <jayson@basanes.net>
2020-04-25 14:55:52 -0600

    Add generated ProductImage with CopiableTests

---
+++ Networking/Networking/Model/Copiable/Generated/ProductImage+Copiable.generated.swift
@@ -0,0 +1,30 @@
+// Generated using Sourcery 0.17.0 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+
+extension ProductImage {
+    public func copy(
+        imageID: CopiableProp<Int64> = .copy,
+        dateCreated: CopiableProp<Date> = .copy,
+        dateModified: NullableCopiableProp<Date> = .copy,
+        src: CopiableProp<String> = .copy,
+        name: NullableCopiableProp<String> = .copy,
+        alt: NullableCopiableProp<String> = .copy
+    ) -> ProductImage {
+        let imageID = imageID ?? self.imageID
+        let dateCreated = dateCreated ?? self.dateCreated
+        let dateModified = dateModified ?? self.dateModified
+        let src = src ?? self.src
+        let name = name ?? self.name
+        let alt = alt ?? self.alt
+
+        return ProductImage(
+            imageID: imageID,
+            dateCreated: dateCreated,
+            dateModified: dateModified,
+            src: src,
+            name: name,
+            alt: alt
+        )
+    }
+}
